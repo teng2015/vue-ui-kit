8ca2daeeccadbc0a059c01f8f0f36a6d540b3daf
Chris Pan <panteng@outlook.com>
2016-01-15 19:02:20 +0800

    Add views for table demo and checkbox demo.

--- app/source/javascripts/views/table-demo/index.js
+++ app/source/javascripts/views/table-demo/index.js
@@ -1,3 +1,99 @@
 module.exports = {
-    template: require('./index.html')
+    template: require('./index.html'),
+    data: function () {
+        return {
+            players: [
+                {
+                    name: 'Kobe Bryant',
+                    number: '24',
+                    position: 'SF',
+                    birth: '1978-08-23'
+                },
+                {
+                    name: 'Roy Hibbert',
+                    number: '17',
+                    position: 'C',
+                    birth: '1986-12-11'
+                },
+                {
+                    name: 'Lou Williams',
+                    number: '23',
+                    position: 'SG',
+                    birth: '1986-12-11'
+                },
+                {
+                    name: 'Nick Young',
+                    number: '0',
+                    position: 'SG',
+                    birth: '1985-06-01'
+                },
+                {
+                    name: "D'Angelo Russell",
+                    number: '1',
+                    position: 'PG',
+                    birth: '1996-02-23'
+                },
+                {
+                    name: 'Julius Randle',
+                    number: '30',
+                    position: 'PF',
+                    birth: '1994-11-29'
+                },
+                {
+                    name: 'Brandon Bass',
+                    number: '2',
+                    position: 'PF',
+                    birth: '1985-04-30'
+                },
+                {
+                    name: 'Ryan Kelly',
+                    number: '4',
+                    position: 'PF',
+                    birth: '1991-04-09'
+                },
+                {
+                    name: 'Metta World Peace',
+                    number: '37',
+                    position: 'SF',
+                    birth: '1993-01-01'
+                },
+                {
+                    name: 'Larry Nance Jr.',
+                    number: '7',
+                    position: 'PF',
+                    birth: '1986-12-11'
+                },
+                {
+                    name: 'Robert Sacre',
+                    number: '50',
+                    position: 'C',
+                    birth: '1989-06-06'
+                },
+                {
+                    name: 'Jordan Clarkson',
+                    number: '6',
+                    position: 'PG',
+                    birth: '1992-06-07'
+                },
+                {
+                    name: 'Tarik Black',
+                    number: '28',
+                    position: 'PF',
+                    birth: '1991-11-22'
+                },
+                {
+                    name: 'Anthony Brown',
+                    number: '3',
+                    position: 'SF',
+                    birth: '1992-10-10'
+                },
+                {
+                    name: 'Marcelo Huertas',
+                    number: '9',
+                    position: 'PG',
+                    birth: '1983-05-25'
+                }
+            ]
+        }
+    }
 };
