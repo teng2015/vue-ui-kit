bb9f8d2b8e19694319f3d610cb60f70a442c1e47
Chris Pan <panteng@outlook.com>
2016-01-26 00:18:44 +0800

    Add sort-field directive and seach-field directive. Search-field is not finished yet.

--- app/src/javascripts/components/table/index.js
+++ app/src/javascripts/components/table/index.js
@@ -8,11 +8,15 @@ module.exports = {
     data: function () {
         return {
             rawData: [],
+            searchText: '',
             allChecked: false,
-            sort: {
-                field: '',
-                order: ''
-            }
+            sortCondition: {},
+            filterCondition: [
+                {
+                    field: '',
+                    value: ''
+                }
+            ]
         }
     },
     created: function () {
@@ -48,35 +52,70 @@ module.exports = {
         },
         filteredRawData: function () {
             var vm = this;
-            return vm.rawData;
+
+            return _.filter(vm.rawData, function (item) {
+                return item.name.toLowerCase().indexOf(vm.filterCondition[0].value) !== -1;
+            });
         },
         thisPageData: function () {
             var vm = this;
             return vm.filteredRawData;
         }
     },
-    methods: {
-        sortBy: function (sortField) {
-            var vm = this;
+    directives: {
+        'search-field': {
+            bind: function () {
+                var _this = this;
+                var searchField = _this.expression;
+                var $el = $(_this.el);
 
-            if (vm.sort.field === sortField) {
-                vm.sort.order = vm.sort.order === 'desc' ? 'asc' : 'desc';
-            } else {
-                vm.sort.field = sortField;
-                vm.sort.order = 'desc';
-            }
-
-            if (vm.sort.order === 'desc') {
-                vm.rawData = _.sortBy(vm.rawData, function (item) {
-                    return item[sortField];
+                $el.on('keyup', function () {
+                    _this.vm.filterCondition[0].field = searchField;
+                    _this.vm.filterCondition[0].value = $el.val();
                 });
-            } else {
-                vm.rawData = _.sortBy(vm.rawData, function (item) {
-                    return item[sortField];
-                }).reverse();
+            },
+            unbind: function () {
+                $el.off('keyup');
             }
+        },
+        'sort-field': {
+            bind: function () {
+                var _this = this;
+                var sortField = _this.expression;
+                var $el = $(_this.el);
 
-            console.log(vm.rawData);
+                $el.addClass('sortable');
+
+                $el.on('click', function () {
+                    $el.siblings('th').removeClass('desc asc');
+
+                    if (_this.vm.sortCondition.field === sortField) {
+                        _this.vm.sortCondition.order = _this.vm.sortCondition.order === 'desc' ? 'asc' : 'desc';
+                    } else {
+                        _this.vm.sortCondition.field = sortField;
+                        _this.vm.sortCondition.order = 'desc';
+                    }
+
+                    if (_this.vm.sortCondition.order === 'desc') {
+                        _this.vm.rawData = _.sortBy(_this.vm.rawData, function (item) {
+                            return item[sortField];
+                        }).reverse();
+                        $el.removeClass('asc').addClass('desc');
+                    } else {
+                        _this.vm.rawData = _.sortBy(_this.vm.rawData, function (item) {
+                            return item[sortField];
+                        });
+                        $el.removeClass('desc').addClass('asc');
+                    }
+                });
+            },
+            unbind: function () {
+                var _this = this;
+                $(_this.el)
+                    .off('click')
+                    .removeClass('sortable desc asc');
+            }
         }
-    }
+    },
+    components: {}
 };
